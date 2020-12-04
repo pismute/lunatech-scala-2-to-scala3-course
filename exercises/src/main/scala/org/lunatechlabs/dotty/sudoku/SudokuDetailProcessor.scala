cf12165ceb50e3c1a9dac453be7d7ab9d7894f3b
Changwoo Park <pismute@gmail.com>
2020-12-04 13:00:00 +0100

    given

--- exercises/src/main/scala/org/lunatechlabs/dotty/sudoku/SudokuDetailProcessor.scala
+++ exercises/src/main/scala/org/lunatechlabs/dotty/sudoku/SudokuDetailProcessor.scala
@@ -32,19 +32,19 @@ object SudokuDetailProcessor:
     def sendUpdate(id: Int, cellUpdates: CellUpdates)(using sender: ActorRef[Response]): Unit
     def processorName(id: Int): String
 
-  implicit val rowUpdateSender: UpdateSender[Row] = new UpdateSender[Row] {
+  given UpdateSender[Row] = new UpdateSender[Row] {
     override def sendUpdate(id: Int, cellUpdates: CellUpdates)(using sender: ActorRef[Response]): Unit =
       sender ! RowUpdate(id, cellUpdates)
     def processorName(id: Int): String = s"row-processor-$id"
   }
 
-  implicit val columnUpdateSender: UpdateSender[Column] = new UpdateSender[Column] {
+  given UpdateSender[Column] = new UpdateSender[Column] {
     override def sendUpdate(id: Int, cellUpdates: CellUpdates)(using sender: ActorRef[Response]): Unit =
       sender ! ColumnUpdate(id, cellUpdates)
     def processorName(id: Int): String = s"col-processor-$id"
   }
 
-  implicit val blockUpdateSender: UpdateSender[Block] = new UpdateSender[Block] {
+  given UpdateSender[Block] = new UpdateSender[Block] {
     override def sendUpdate(id: Int, cellUpdates: CellUpdates)(using sender: ActorRef[Response]): Unit =
       sender ! BlockUpdate(id, cellUpdates)
     def processorName(id: Int): String = s"blk-processor-$id"
@@ -103,4 +103,3 @@ class SudokuDetailProcessor[DetailType <: SudokoDetailType : UpdateSender] priva
   private def isFullyReduced(state: ReductionSet): Boolean =
     val allValuesInState = state.flatten
     allValuesInState == allValuesInState.distinct
-
